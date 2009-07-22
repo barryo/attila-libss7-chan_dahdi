a4fc53b4870dce9e7664e32bd88e29a84322b485
adomjan <adomjan@86874229-a162-0410-934b-be3dcc1832db>
2009-07-22 10:26:09 +0000

    dahdi_read limit
    git-svn-id: https://observer.router.hu/repos_pub/chan_dahdi@49 86874229-a162-0410-934b-be3dcc1832db

--- trunk/chan_dahdi.c
+++ trunk/chan_dahdi.c
@@ -5522,9 +5522,17 @@ static struct ast_frame  *dahdi_read(struct ast_channel *ast)
 	int index;
 	void *readbuf;
 	struct ast_frame *f;
+	int i = 0;
 
-	while (ast_mutex_trylock(&p->lock)) {
+	while (ast_mutex_trylock(&p->lock) && i < 1000) {
 		CHANNEL_DEADLOCK_AVOIDANCE(ast);
+		i++;
+	}
+
+	if (i == 1000) {
+		ast_log(LOG_ERROR, "Couldn't lock DAHDI pvt...\n");
+		f = __dahdi_exception(ast);
+		return f;
 	}
 
 	index = dahdi_get_index(ast, p, 0);
